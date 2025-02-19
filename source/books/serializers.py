14cd24ad2a6afb6ccf9021e4d402dfdf50204f67
ABM1204 <melisovbz@gmail.com>
2025-02-19 13:31:00 +0600

    Написал сериализаторы для моделей

---
+++ source/books/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+
+from .models import Book, Author, FavoriteBook
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+
+class FavoriteBookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FavoriteBook
+        fields = '__all__'
+
