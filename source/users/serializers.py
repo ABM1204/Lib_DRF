a57d841ee34c8029f1d92cab64e9fcb6616f0b1c
ABM1204 <melisovbz@gmail.com>
2025-02-21 16:53:39 +0600

    После регистрации пользователя, невозможен был вход. Пофиксил.

--- source/users/serializers.py
+++ source/users/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
-
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
@@ -10,12 +9,20 @@ class UserSerializer(serializers.ModelSerializer):
         fields = '__all__'
         extra_kwargs = {'password': {'write_only': True}}
 
-        def create(self, validated_data):
-            password = validated_data.pop('password', None)
-            user = User(**validated_data)
-            if password:
-                user.set_password(password)
-            user.is_active = True
-            user.save()
-            return user
+    def create(self, validated_data):
+        groups = validated_data.pop('groups', [])
+        permissions = validated_data.pop('user_permissions', [])
+        password = validated_data.pop('password', None)
 
+        user = User(**validated_data)
+        if password:
+            user.set_password(password)
+        user.is_active = True
+        user.save()
+
+        if groups:
+            user.groups.set(groups)
+        if permissions:
+            user.user_permissions.set(permissions)
+
+        return user
